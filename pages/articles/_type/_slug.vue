a006883d626e5c12c4b8fd3de7fab48726e4313e
WhyK <windchimeyk@gmail.com>
2020-10-31 13:59:16 +0900

    feat: add toc list

--- pages/articles/_type/_slug.vue
+++ pages/articles/_type/_slug.vue
@@ -2,6 +2,14 @@
   <article class="article">
     <h1 class="article__title">{{ content.title }}</h1>
     <time class="article__date" :datetime="content.date">{{ content.date }}</time>
+    <details class="toc">
+      <summary class="toc__title">Index</summary>
+      <ul class="toc-list">
+        <li v-for="link in content.toc" :key="link.id" :class="['toc-list__item', `toc-list__item--0${link.depth}`]">
+          <a :href="`#${link.id}`">{{ link.text }}</a>
+        </li>
+      </ul>
+    </details>
     <nuxt-content :document="content" />
   </article>
 </template>
@@ -62,5 +70,20 @@ export default Vue.extend({
   display: inline-block;
   font-size: 1.5rem;
   margin-bottom: 30px;
+}
+.toc {
+  margin-bottom: 30px;
+}
+.toc__title {
+  font-size: 1.5rem;
+  cursor: pointer;
+}
+.toc-list__item {
+  &--03 {
+    margin-left: 1em;
+  }
+  &--04 {
+    margin-left: 2em;
+  }
 }
 </style>

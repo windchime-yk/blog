3cd8ec2c436c08a7c98a45edd5e6e288e58573aa
WhyK <windchimeyk@gmail.com>
2024-04-11 15:41:09 +0900

    ci: fix setup step

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "WhyK Log";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "WhyKが技術や雑談を記録するブログです。";
